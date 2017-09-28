0cdd29765c7af85e191c34bfab497582f5d8cb3d
Nessa <nessa_m00re@yahoo.com>
2017-09-28 03:25:50 +0000

    Rewrite routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items
-#    Prefix Verb   URI Pattern               Controller#Action
-#     items GET    /items(.:format)          items#index
-#           POST   /items(.:format)          items#create
-#  new_item GET    /items/new(.:format)      items#new
-# edit_item GET    /items/:id/edit(.:format) items#edit
-#      item GET    /items/:id(.:format)      items#show
-#           PATCH  /items/:id(.:format)      items#update
-#           PUT    /items/:id(.:format)      items#update
-#           DELETE /items/:id(.:format)      items#destroy
+  get '/items'        => 'items#index'
+  post '/items'       => 'items#create'
+  get '/items/:id'    => 'items#show'
+  post '/items/:id'   => 'items#update'
+  delete '/items/:id' => 'items#destroy'
 end
